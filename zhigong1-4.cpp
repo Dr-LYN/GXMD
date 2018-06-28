06e1859f2c91c15013a486d3f4be75f5e01a47bd
Dr-LYN <40430107+Dr-LYN@users.noreply.github.com>
2018-06-28 17:00:45 +0800

    Add files via upload

--- zhigong1-4.cpp
+++ zhigong1-4.cpp
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<iostream>
 #include<conio.h>
@@ -14,24 +13,23 @@ struct zggz{
 	float wage_yf;
 	float taxfee;
 	float wage_sf;
-}zgs[999],*zg;
+}zgs[999];
 	int n=0;
 	void saveone(int i);
 void read()    //读取职工工资数据
 {
 	 FILE *fp;
-     zg=zgs;
-     if((fp=fopen("gz.dat","rb+"))==NULL)
+     if((fp=fopen("gz.dat","rb"))==NULL)
      {
           printf("不能打开文件\n");
 		  getch();
-          exit(-1);
+          exit(1);
      }
-	 rewind(fp);
-	 for(int i=0; i<999; i++,zg++)  //判断几条数据
+	 //rewind(fp);
+	 for(int i=0; i<999; i++)  //判断几条数据
 	 {  
-		 fscanf(fp,"%s %s %f %f %f %f %f %f %f\n",zg->num,zg->name,&(zg->wage_gw),&(zg->wage_xj),&(zg->wage_zw),&(zg->wage_jx),&(zg->wage_yf),&(zg->taxfee),&(zg->wage_sf));
-		 printf("%s %s %f %f %f %f %f %f %f\n",zg->num,zg->name,zg->wage_gw,zg->wage_xj,zg->wage_zw,zg->wage_jx,zg->wage_yf,zg->taxfee,zg->wage_sf);
+		 fscanf(fp,"%s %s %f %f %f %f %f %f %f\n",zgs[i].num,zgs[i].name,&(zgs[i].wage_gw),&(zgs[i].wage_xj),&(zgs[i].wage_zw),&(zgs[i].wage_jx),&(zgs[i].wage_yf),&(zgs[i].taxfee),&(zgs[i].wage_sf));
+		 printf("%s %s %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",zgs[i].num,zgs[i].name,zgs[i].wage_gw,zgs[i].wage_xj,zgs[i].wage_zw,zgs[i].wage_jx,zgs[i].wage_yf,zgs[i].taxfee,zgs[i].wage_sf);
 		 n++;
 		 if(feof(fp))
 		 {
@@ -46,19 +44,20 @@ void read()    //读取职工工资数据
 void write()
 {
 	FILE *fp; 
- if((fp=fopen("gz.dat","rb"))==NULL) 
- {
+	if((fp=fopen("gz.dat","wb+"))==NULL) 
+	{
 	 printf("不能打开 \n"); 
 	 getch();
-	 exit(-1); 
- } 
- rewind(fp);
- for(int i=0;i<n;i++)
- {
-	 fwrite(&zgs[i],sizeof(struct zggz),1,fp);
- }
- printf("成功\n");
- fclose(fp); 
+	 exit(1); 
+	} 
+	rewind(fp);
+	for(int i=0;i<n;i++)
+	{
+		fprintf(fp,"%s %s %f %f %f %f %f %f %f\n",zgs[i].num,zgs[i].name,zgs[i].wage_gw,zgs[i].wage_xj,zgs[i].wage_zw,zgs[i].wage_jx,zgs[i].wage_yf,zgs[i].taxfee,zgs[i].wage_sf);
+		//fwrite(&zgs[i],sizeof(struct zggz),1,fp);
+	}
+	printf("成功\n");
+	fclose(fp); 
 }
 void find()
 {
@@ -70,7 +69,7 @@ void find()
 		if(strcmp(zgs[i].num,gonghao)==0)
 		{
 			printf("有");
-			printf("%s\n",zgs[i].num);
+			printf("%s %s %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",zgs[i].num,zgs[i].name,zgs[i].wage_gw,zgs[i].wage_xj,zgs[i].wage_zw,zgs[i].wage_jx,zgs[i].wage_yf,zgs[i].taxfee,zgs[i].wage_sf);
 			getch();
 			break;	
 		}
@@ -80,53 +79,54 @@ void find()
 }
 void list()
 {
-	zg=zgs;
 	char jieshu[10];
 	jieshu[0]=0;
-		printf("工号   姓名   岗位工资  薪级工资   职务津贴   绩效工资  应发工资  个人所得税   实发工资\n");
-	for(int i=0;i<999;i++,zg++)
+	printf("工号      姓名      岗位工资      薪级工资       职务津贴      绩效工资     应发工资      个人所得税      实发工资\n");
+	for(int i=0;i<999;i++)
 	{	
-		printf("%s %s %f %f %f %f %f %f %f\n",zg->num,zg->name,zg->wage_gw,zg->wage_xj,zg->wage_zw,zg->wage_jx,zg->wage_yf,zg->taxfee,zg->wage_sf);
+		printf("%s\t %s\t %8.2f\t  %8.2f  %8.2f\t %8.2f\t  %8.2f\t   %8.2f\t  %8.2f\t\n",zgs[i].num,zgs[i].name,zgs[i].wage_gw,zgs[i].wage_xj,zgs[i].wage_zw,zgs[i].wage_jx,zgs[i].wage_yf,zgs[i].taxfee,zgs[i].wage_sf);
 		if(strcmp(zgs[i+1].num,jieshu)==0) break;
 	}
 }
 void modify()
 {
-	
 	char gonghao[20];
 	printf("\n请输入职工编号:");
 	//zg=zgs;
 	gets(gonghao);
-	for(int i=0; i<999; i++,zg++)  
+	for(int i=0; i<999; i++)  
 	{
 		if(strcmp(zgs[i].num,gonghao)==0)
 		{
 			printf("请输入职工信息:\n");
 			//scanf("%s %s %f %f %f %f %f %f %f\n",zg->num,zg->name,&(zg->wage_gw),&(zg->wage_xj),&(zg->wage_zw),&(zg->wage_jx),&(zg->wage_yf),&(zg->taxfee),&(zg->wage_sf));
 			printf("工号：");
-			scanf("%s",zg->num);
+			scanf("%s",zgs[i].num);
 			printf("姓名：");
-			scanf("%s",zg->name);
+			scanf("%s",zgs[i].name);
+			printf("%s",zgs[i].name);
 			printf("岗位工资：");
-			scanf("%f",&(zg->wage_gw));
+			scanf("%f",&(zgs[i].wage_gw));
 			printf("薪级工资：");
-			scanf("%f",&(zg->wage_xj));
+			scanf("%f",&(zgs[i].wage_xj));
 			printf("职务津贴：");
-			scanf("%f",&(zg->wage_zw));
+			scanf("%f",&(zgs[i].wage_zw));
 			printf("绩效工资：");
-			scanf("%f",&(zg->wage_jx));
+			scanf("%f",&(zgs[i].wage_jx));
 			printf("应发工资：");
-			scanf("%f",&(zg->wage_yf));
+			scanf("%f",&(zgs[i].wage_yf));
 			printf("个人所得税：");
-			scanf("%f",&(zg->taxfee));
+			scanf("%f",&(zgs[i].taxfee));
 			printf("实发工资：");
-			scanf("%f",&(zg->wage_sf));
-			getch();
+			scanf("%f",&(zgs[i].wage_sf));
+			getch();write();
 			break;	
 		}
 	}
+
 }
-void saveone(int i)
+
+/*void saveone(int i)
 {
 	FILE *fp; 
 	if((fp=fopen("gz.dat","wb+"))==NULL) 
@@ -137,13 +137,78 @@ void saveone(int i)
 	} 
 	fwrite(&zgs[i],sizeof(struct zggz),1,fp);
 	fclose(fp);
+}*/
+void add()
+{
+	FILE *fp; 
+	if((fp=fopen("gz.dat","ab+"))==NULL) 
+	{
+	 printf("不能打开 \n"); 
+	 getch();
+	 exit(1); 
+	} 
+	n=n+1;
+	printf("增加第：%d\n",n);
+	printf("工号：");
+	scanf("%s",zgs[n].num);
+	printf("姓名：");
+	scanf("%s",zgs[n].name);
+	printf("岗位工资：");
+	scanf("%f",&(zgs[n].wage_gw));
+	printf("薪级工资：");
+	scanf("%f",&(zgs[n].wage_xj));
+	printf("职务津贴：");
+	scanf("%f",&(zgs[n].wage_zw));
+	printf("绩效工资：");
+	scanf("%f",&(zgs[n].wage_jx));
+	/*printf("应发工资：");
+	scanf("%f",&(zgs[n].wage_yf));*/
+	zgs[n].wage_yf=zgs[n].wage_gw+zgs[n].wage_xj+zgs[n].wage_zw+zgs[n].wage_jx;
+	printf("个人所得税：");
+	scanf("%f",&(zgs[n].taxfee));
+	printf("实发工资：");
+	scanf("%f",&(zgs[n].wage_sf));
+	//fwrite(&zgs[2],sizeof(struct zggz),1,fp);
+	fprintf(fp,"%s %s %f %f %f %f %f %f %f",zgs[n].num,zgs[n].name,zgs[n].wage_gw,zgs[n].wage_xj,zgs[n].wage_zw,zgs[n].wage_jx,zgs[n].wage_yf,zgs[n].taxfee,zgs[n].wage_sf);
+}
+void del()
+{	
+	char gonghao[20];
+	char c1='0';
+	printf("\n请输入要删除的职工编号:");
+	gets(gonghao);
+	for(int i=0; i<999; i++)  
+	{
+		if(strcmp(zgs[i].num,gonghao)==0)
+		{
+			printf("%s %s %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",zgs[i].num,zgs[i].name,zgs[i].wage_gw,zgs[i].wage_xj,zgs[i].wage_zw,zgs[i].wage_jx,zgs[i].wage_yf,zgs[i].taxfee,zgs[i].wage_sf);
+			printf("确定要删除吗？确认按1 :");
+			c1=getchar();
+			getchar();
+			if(c1=='1')
+			{
+				for(i;i<=n;i++)
+				{
+					zgs[i]=zgs[i+1];
+				}printf("\n成功删除!\n");n--;write();
+				
+			} 	
+			else 
+			{
+				printf("\n取消删除!\n");
+			}
+		}
+	}
 }
 int main()
 {
 	read();
+	//write();
 	//find();
 	//modify();
-	//write();
-	//list();
+	list();
+	//add();
+	//del();
+	//grsds()
 	return 0;
 }
